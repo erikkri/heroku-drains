bd49e02307ad7d90706df70946c007b33559b390
Erik Kristensen <erik.kristensen@schibsted.no>
2018-11-21 15:27:40 +0100

    Removed helloworld. List heroku apps.

--- src/commands/add/sumologic.js
+++ src/commands/add/sumologic.js
@@ -1,10 +1,15 @@
 const {Command, flags} = require('@oclif/command')
+const Heroku = require('heroku-client')
+const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN })
 
 class SumologicCommand extends Command {
   async run() {
     const {flags} = this.parse(SumologicCommand)
     const team = flags.team || 'world'
     this.log(`hello ${team} from /Users/erikrist/git/heroku-drains/src/commands/add/sumologic.js`)
+    heroku.get('/apps').then(apps => {
+      apps.forEach((app) => this.log(app.name))
+    })
   }
 }
 
@@ -16,7 +21,7 @@ The URL for the corresponding source will be added as a log drain to the corresp
 SumologicCommand.flags = {
   team: flags.string({char: 't', description: 'Heroku team where sumologic drains will be added.'}),
   collector: flags.string({char: 'c', description: 'Sumologic collector which will be used.'}),
-  team: flags.string({char: 's', description: 'Source category which these collectors will be added to.'}),
+  category: flags.string({char: 's', description: 'Source category which these collectors will be added to.'}),
 }
 
 module.exports = SumologicCommand
